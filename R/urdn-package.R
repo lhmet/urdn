5df1034d34de6f0504c6d8c777298608b4fc3d81
lhmet <lhmet@ufsm.br>
2018-02-03 21:46:04 -0200

    unnecessary

--- R/urdn-package.R
+++ R/urdn-package.R
@@ -1,2 +1,7 @@
-#' @keywords internal
+#' Generate R documentation from inline comments.
+#'
+#' urdn allows you to upscaling river drainage networks .
+#'
+#' The only function you're likely to need from \pkg{urdn} is
+#' \code{\link{cotat_plus}}.
 "_PACKAGE"
