ab33bd1977292c3434c7279d0caf347ecd227b0d
lhmet <lhmet@ufsm.br>
2018-02-07 05:38:35 -0200

    keep NAs

--- data-raw/prep-data-ex.R
+++ data-raw/prep-data-ex.R
@@ -5,7 +5,7 @@ area_hres <- raster::raster(area_file)
 area_hres
 dir_hres <- raster::raster(dir_file)
 dir_hres
-dir_hres[is.na(dir_hres)] <- -9999
-area_hres[is.na(area_hres)] <- -9999
+dir_hres[dir_hres < 0] <- NA
+area_hres[area_hres < 0] <- NA
 usethis::use_data(area_hres, dir_hres, overwrite = TRUE)
 
